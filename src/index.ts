53248bab6e818e9a4d51cf1bf4dc40d736185490
israelli <li18566230175@gmail.com>
2023-10-16 11:14:38 +0800

    feat: eslint format

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-
 import { debounce, merge } from 'lodash';
 import tippy, { Instance, Props } from 'tippy.js';
 
@@ -6,22 +5,22 @@ declare global {
   interface Window {
     $TEXT_OVERFLOW_POS: HTMLElement;
   }
-}
+};
 
 export interface ITextoverflowOption {
   target: HTMLElement | string;
-  showTooltip?: boolean,
-  prefix?: string,
-  delay?: number,
-  contentAttributes?: string[]
-}
+  showTooltip?: boolean;
+  prefix?: string;
+  delay?: number;
+  contentAttributes?: string[];
+};
 
 const defaultOption: ITextoverflowOption = {
   target: document.body,
   showTooltip: true,
   prefix: 'bk-txt-overflow',
   contentAttributes: ['value', 'title', 'innerHTML'],
-  delay: 500
+  delay: 500,
 };
 
 function resolveTarget(target?: HTMLElement | string) {
@@ -38,11 +37,11 @@ function resolveTarget(target?: HTMLElement | string) {
   }
 
   return target;
-}
+};
 
 const resolveOptions = (option?: ITextoverflowOption) => {
   return merge(defaultOption || {}, option);
-}
+};
 
 const defaultTargetNodeConfig = {
   tooltip_title: '',
@@ -57,7 +56,7 @@ const defaultTargetNodeConfig = {
 export const setTextOverflowTooltip = (options: ITextoverflowOption) => {
   let tippyInstance: Instance<Props> | null = null;
   let targetNode: HTMLElement | Element | undefined | null;
-  let targetConfig = {...defaultTargetNodeConfig};
+  let targetConfig = { ...defaultTargetNodeConfig };
   let showPopInstanTimer: number;
   const hanldeMouseEnter = debounce((evt) => {
     const x = evt.clientX;
@@ -76,16 +75,16 @@ export const setTextOverflowTooltip = (options: ITextoverflowOption) => {
       const checkList = [
         ['textOverflow', 'ellipsis'],
         ['overflow', 'hidden'],
-        ['whiteSpace', 'nowrap']
+        ['whiteSpace', 'nowrap'],
       ];
 
       if (targetNode) {
         const style = window.getComputedStyle(targetNode);
-        return checkList.every(item => style[item[0]] === item[1]);
+        return checkList.every((item) => style[item[0]] === item[1]);
       }
 
       return false;
-    }
+    };
 
     if (hasOverflowEllipsis(targetNode as HTMLElement) && isOverflow()) {
       showTooltip();
@@ -102,8 +101,8 @@ export const setTextOverflowTooltip = (options: ITextoverflowOption) => {
     showPopInstanTimer && clearTimeout(showPopInstanTimer);
     tippyInstance?.hide();
     targetNode?.removeEventListener('mouseleave', handleMouseleave);
-    Object.assign(targetConfig, {...defaultTargetNodeConfig});
-  }
+    Object.assign(targetConfig, { ...defaultTargetNodeConfig });
+  };
 
   const resolveTargetAttribute = (attributeName: string, defaultValue: boolean | string | null | number = null) => {
     const attrName = `${options.prefix}-${attributeName}`;
@@ -112,23 +111,23 @@ export const setTextOverflowTooltip = (options: ITextoverflowOption) => {
       if (value === null || value === '') {
         return defaultValue;
       }
-    
+
       return value;
     }
-    
+
     return undefined;
-  }
+  };
 
   const setTargetNodeAttribute = (name: string, value: string) => {
     const attrName = `${options.prefix}-${name}`;
     targetNode?.setAttribute(attrName, value);
-  }
+  };
 
   const removeNodeAttribute = (name: string) => {
     const attrName = `${options.prefix}-${name}`;
     targetNode?.removeAttribute(attrName);
-  }
-  
+  };
+
   const resolveTargetConfig = () => {
     Object.assign(targetConfig, {
       tooltip_title: resolveTargetAttribute('title'),
@@ -139,7 +138,7 @@ export const setTextOverflowTooltip = (options: ITextoverflowOption) => {
       tooltip_delay: resolveTargetAttribute('delay', options.delay),
       system_title: resolveTargetAttribute('system', true),
     });
-  }
+  };
 
   const getContent = (target: HTMLElement) => {
     const { tooltip_title, tooltip_target } = targetConfig;
@@ -152,12 +151,12 @@ export const setTextOverflowTooltip = (options: ITextoverflowOption) => {
       if (Array.isArray(options.contentAttributes)) {
         let i = 0;
         let result = tooltip_title;
-        while(i < options.contentAttributes.length) {
+        while (i < options.contentAttributes.length) {
           const attr = options.contentAttributes[i];
-          if (target[attr] !== undefined) {
+          if (target[attr] !== undefined && target[attr] !== null && target[attr] !== '') {
             result = target[attr];
           }
-          
+
           if (target.hasAttribute(attr)) {
             result = `${target.getAttribute(attr)}`;
             break;
@@ -167,15 +166,16 @@ export const setTextOverflowTooltip = (options: ITextoverflowOption) => {
         }
 
         return result;
-      };
+      }
     }
-  }
+  };
 
   const showTooltip = () => {
     // 需要显示的内容
     // 如果设置了data-show-title优先展示，否则展示当前子元素内容
     let targetValue: string | Element | undefined | null;
-    const { tooltip_title, tooltip_target, tooltip_disabled, tooltip_theme, tooltip_delay, system_title } = targetConfig;
+    const { tooltip_title, tooltip_target, tooltip_disabled, tooltip_theme, tooltip_delay, system_title } =
+      targetConfig;
     if (tooltip_disabled) {
       return;
     }
@@ -186,28 +186,27 @@ export const setTextOverflowTooltip = (options: ITextoverflowOption) => {
       showPopInstanTimer && clearTimeout(showPopInstanTimer);
       tippyInstance?.destroy();
       showPopInstanTimer = setTimeout(() => {
-        tippyInstance = tippy((targetNode as HTMLElement), {
+        tippyInstance = tippy(targetNode as HTMLElement, {
           content: targetValue || 'null',
           allowHTML: true,
           arrow: true,
           theme: `${tooltip_theme || 'bk-dark'}`,
           trigger: 'manual',
           onShow: (instance) => {
-            instance.popper?.classList.add('bk-text-overflow-popper')
+            instance.popper?.classList.add('bk-text-overflow-popper');
           },
           onHidden: () => {
             tippyInstance = null;
             replaceNodeAttribute('data-title', 'title', targetNode as HTMLElement);
-          }
+          },
         });
-        
+
         replaceNodeAttribute('title', 'data-title', targetNode as HTMLElement);
         tippyInstance.show();
       }, Number(tooltip_delay));
     } else {
-      targetValue = tooltip_title as string
-        || ((targetNode as HTMLInputElement).value
-        || (targetNode as HTMLElement)?.innerText);
+      targetValue =
+        (tooltip_title as string) || (targetNode as HTMLInputElement).value || (targetNode as HTMLElement)?.innerText;
 
       if (!targetNode?.hasAttribute('title')) {
         targetNode?.setAttribute('title', targetValue);
@@ -215,18 +214,17 @@ export const setTextOverflowTooltip = (options: ITextoverflowOption) => {
         resolveTargetConfig();
       }
     }
-  }
+  };
 
   resolveTarget(options.target)?.addEventListener('mouseover', hanldeMouseEnter);
-}
+};
 
 function replaceNodeAttribute(source: string, target: string, node?: HTMLElement | null) {
   if (node?.hasAttribute(source)) {
     node.setAttribute(target, `${node.getAttribute(source)}`);
     node.removeAttribute(source);
-  }  
-}
-
+  }
+};
 
 /**
  * Whether the text content is clipped due to CSS overflow, as in showing `...`.
@@ -249,31 +247,33 @@ export function hasOverflowEllipsis(element: HTMLElement) {
   }
 
   return element.offsetWidth < element.scrollWidth;
-}
+};
 
-export function copyCssStyleToTargetNode (sourceNode: HTMLElement, targetNode: HTMLElement, propertyList: string[] = []) {
+export function copyCssStyleToTargetNode(
+  sourceNode: HTMLElement,
+  targetNode: HTMLElement,
+  propertyList: string[] = []
+) {
   const styles = window.getComputedStyle(sourceNode);
   if (propertyList.length > 0) {
-    const cssText = propertyList.reduce((css, propertyName) =>
-      `${css}${propertyName}:${styles.getPropertyValue(propertyName)};`, '');
-    targetNode.style.cssText = cssText
+    const cssText = propertyList.reduce(
+      (css, propertyName) => `${css}${propertyName}:${styles.getPropertyValue(propertyName)};`,
+      ''
+    );
+    targetNode.style.cssText = cssText;
     return;
   }
   if (styles.cssText !== '') {
     targetNode.style.cssText = styles.cssText;
   } else {
-      const cssText = Object.values(styles).reduce(
-          (css, propertyName) =>
-              `${css}${propertyName}:${styles.getPropertyValue(
-                  propertyName
-              )};`
-      );
+    const cssText = Object.values(styles).reduce(
+      (css, propertyName) => `${css}${propertyName}:${styles.getPropertyValue(propertyName)};`
+    );
 
-      targetNode.style.cssText = cssText
+    targetNode.style.cssText = cssText;
   }
-}
-
+};
 
 export default (options: ITextoverflowOption) => {
   setTextOverflowTooltip(resolveOptions(options));
-}
+};
